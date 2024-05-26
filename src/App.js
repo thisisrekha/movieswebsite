06275a5007778c278692d0f302e3d058bf327b17
thisisrekha <kondurinaga@iitbhilai.ac.in>
2024-05-27 02:21:54 +0530

    created boilerplate for React

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import MoviesComponent from './components/MoviesComponent';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <MoviesComponent />
+      </header>
+    </div>
+  );
+}
+
+export default App;
