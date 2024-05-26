b1aefeca87cb1b1a103f2c2ea52a67a1cedc6129
thisisrekha <kondurinaga@iitbhilai.ac.in>
2024-05-27 02:49:11 +0530

    modified App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import MoviesComponent from './components/MoviesComponent';
+import FavoriteMoviesList from './components/FavoriteMoviesList';
+import './App.css';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <MoviesComponent />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <nav className="navbar">
+            <ul className="nav-links">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/favorites">Favorite Movies</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<MoviesComponent />} />
+            <Route path="/favorites" element={<FavoriteMoviesList />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
