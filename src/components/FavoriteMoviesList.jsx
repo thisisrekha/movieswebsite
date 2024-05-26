5080a4dfff73e37672847dafcc05a1123ae66e62
thisisrekha <kondurinaga@iitbhilai.ac.in>
2024-05-27 03:44:58 +0530

    modified FavoriteMoviesList.jsx

--- src/components/FavoriteMoviesList.jsx
+++ src/components/FavoriteMoviesList.jsx
@@ -1,90 +1,3 @@
-// import React from 'react';
-// import { useSelector } from 'react-redux';
-
-// const FavoriteMoviesList = () => {
-//   // Get the list of favorite movies from Redux state
-//   const favoriteMovies = useSelector(state => state.favoriteMovies);
-
-//   return (
-//     <div>
-//       <h1>Favorite Movies List</h1>
-//       <ul>
-//         {favoriteMovies.map(movie => (
-//           <li key={movie.id}>
-//             <h2>{movie.movie}</h2>
-//             <p>Rating: {movie.rating}</p>
-//             <img src={movie.image_url} alt={movie.movie} />
-//             <a href={movie.imdb_url} target="_blank" rel="noopener noreferrer">IMDb</a>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default FavoriteMoviesList;
-
-
-
-// import React from 'react';
-// import { useSelector } from 'react-redux';
-
-// const FavoriteMoviesList = () => {
-//   // Get the list of favorite movies from Redux state
-//   const favoriteMovies = useSelector(state => state.favoriteMovies.favorites);
-
-
-//   return (
-//     <div>
-//       <h1>Favorite Movies List</h1>
-//       <ul>
-//         {favoriteMovies.map(movie => (
-//           <li key={movie.id}>
-//             <h2>{movie.movie}</h2>
-//             <p>Rating: {movie.rating}</p>
-//             <img src={movie.image_url} alt={movie.movie} />
-//             <a href={movie.imdb_url} target="_blank" rel="noopener noreferrer">IMDb</a>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default FavoriteMoviesList;
-
-
-
-// import React from 'react';
-// import { useSelector } from 'react-redux';
-
-// const FavoriteMoviesList = () => {
-//   // Get the list of favorite movies from Redux state
-//   const favoriteMovies = useSelector(state => state.favoriteMovies.favorites);
-
-//   return (
-//     <div>
-//       <h1>Favorite Movies List</h1>
-//       <ul>
-//         {favoriteMovies.map(movie => (
-//           <li key={movie.id}>
-//             <h2>{movie.movie}</h2>
-//             <p>Rating: {movie.rating}</p>
-//             <img src={movie.image_url} alt={movie.movie} />
-//             <a href={movie.imdb_url} target="_blank" rel="noopener noreferrer">IMDb</a>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default FavoriteMoviesList;
-
-
-
-
-
 import React from 'react';
 import { useSelector } from 'react-redux';
 import './MoviesComponent.css'; // Use the same CSS for styling
@@ -93,10 +6,17 @@ const FavoriteMoviesList = () => {
   const favoriteMovies = useSelector(state => state.favoriteMovies.favorites);
 
   return (
-    <div>
+    <div class="container">
       <h1>Favorite Movies List</h1>
       <ul className="movies-list">
         {favoriteMovies.map(movie => (
+          <a
+          key={movie.id}
+          href={movie.imdb_url}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="movie-link"
+         >
           <li key={movie.id} className="movie-item">
             <div className="movie-details">
               <h2>{movie.movie}</h2>
@@ -106,9 +26,10 @@ const FavoriteMoviesList = () => {
                 alt={movie.movie}
                 className="movie-image"
               />
-              <a href={movie.imdb_url} target="_blank" rel="noopener noreferrer">IMDb</a>
+              <div><a href={movie.imdb_url}>IMDB</a></div>
             </div>
           </li>
+          </a>
         ))}
       </ul>
     </div>
